15d68620bf716fa0b8fe97793f6e36db9f9b581d
Moim HA <moimhossain@gmail.com>
2021-06-29 13:56:53 +0200

    Script that generates the kubeconfig

--- src/do.sh
+++ src/do.sh
@@ -2,11 +2,15 @@
 
 cd ..
 changedFiles=$(git diff-tree --no-commit-id --name-only -r b7b1b3e)
-echo $changedFiles
-
+workloads=()
 for d in src/governance/*/ ; do  
     if [[ $changedFiles == *"$d"* ]]; then
-    workloadName="$(basename $d)"
-    echo "$workloadName changed (full path: $d)"
+    workloads+=($d)
     fi
+done
+cd src
+for wl in ${workloads[@]}; do
+    workloadName="$(basename $wl)"
+    echo "<<$workloadName>> changed (full path: $wl)"
+    ./generate-kubeconfig.sh $workloadName    
 done
